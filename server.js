06f8dd15d9364a0b271b8ccad1d0e1eff81629ce
omar2-2khaled <UGS.141015UGS.141015@ci.suez.edu.eg>
2024-04-29 21:50:36 +0300

    adding options for mongo

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const port = process.env.PORT;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: true,
+  sslCA: [fs.readFileSync(path.resolve(__dirname, './certs/ca-certificate.crt'))]
+};
 mongoose
-  .connect(process.env.DB)
+  .connect(process.env.DB, options)
   .then(() => {
     console.log('DB Connection Successful');
   });
